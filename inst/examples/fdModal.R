5725f8286c9db817b6109e8857fe1d6804827d09
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-19 15:12:40 -0800

    add more examples to fdModal

--- inst/examples/fdModal.R
+++ inst/examples/fdModal.R
@@ -8,6 +8,28 @@ tooltipData <- list(message = 'This plot shows if residuals are normally distrib
 box1 <- fdBox(title = span("Tooltip", myIcon %>% fdTooltip(.list = tooltipData))) 
 box2 <-  fdBox(title = span("Popover", myIcon %>% fdPopover(.list = tooltipData)))
 box3 <- fdBox(title = span("Modal", myIcon %>% fdModal(.list = tooltipData)))
-tagList(box1, box2, box3) %>%
-  fdPreviewBoard('Modal, Tooltip and Popovers', wrapBox = F)
+myBoxes <- tagList(box1, box2, box3) 
+if (interactive()){
+  fdPreviewBoard(myBoxes, 'Modal, Tooltip and Popovers', wrapBox = F)
+}
+  
 
+
+# Example 2
+title <- span('Hello', 
+  fdIcon('info-circle') %>%
+    fdPopover(
+      message = 'Welcome to flighdeck! The best dashboarding package ever.', 
+      title = 'Hello'
+    )
+)
+if (interactive()){
+  fdBox(
+    p("This is a really cool thing..."),
+    title = title,
+    footer = tags$small('Click on the info sign to view help.'),
+    solidHeader = FALSE,
+    status = 'danger'
+  ) %>%
+    fdPreview(wrapBox = F)
+}
