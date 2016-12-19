4d545d750ad52cdbd2796989b72d2de00338bebb
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-19 15:25:02 -0800

    reorganize example files.

--- inst/examples/fdInfoBox.R
+++ inst/examples/fdInfoBox.R
@@ -1,4 +1,6 @@
 library(flightdeck)
+
+# Example 1
 tagList(
   fdInfoBox('Adjusted R-squared', '86.9%', 
     color = 'blue', icon = fdIcon('chart-line', lib = 'entypo')),
@@ -9,21 +11,16 @@ tagList(
 ) %>%
   fdPreview(wrapBox = F)
 
-library(tidyverse)
-glm(am ~ wt, mtcars, family="binomial") %>%
-  tidy %>%
-  fdTidyTable %>%
-  fdPreview
 
-
+# Example 2: Interactive help embedded in stat box.
+# The message supports markdown.
 msg <- "The adjusted R-squared compares the explanatory power of regression models that contain different numbers of predictors.
 
 Suppose you compare a five-predictor model with a higher R-squared to a one-predictor model. Does the five predictor model have a higher R-squared because it’s better? Or is the R-squared higher because it has more predictors? Simply compare the adjusted R-squared values to find out!
 
 The adjusted R-squared is a modified version of R-squared that has been adjusted for the number of predictors in the model. The adjusted R-squared increases only if the new term improves the model more than would be expected by chance. It decreases when a predictor improves the model by less than expected by chance. The adjusted R-squared can be negative, but it’s usually not.  It is always lower than the R-squared."
 
-d <- lm(mpg ~ ., data = mtcars) %>%
-  glance %>% signif(4)
+
 
 adjRsquared <- span("Adjusted R-squared",
   fdIcon('info-circled', lib = 'entypo') %>%
@@ -31,11 +28,11 @@ adjRsquared <- span("Adjusted R-squared",
 )
 
 tagList(
-  fdInfoBox(adjRsquared, paste0(round(d$adj.r.squared*100, 1), '%'), 
+  fdInfoBox(adjRsquared, '86.9%', 
     color = 'blue', icon = fdIcon('chart-line', lib = 'entypo')),
-  fdInfoBox('F-Statistic', d$statistic,  'on 10 and 21 degrees of freedom', 
+  fdInfoBox('F-Statistic', 13.93,  'on 10 and 21 degrees of freedom', 
     color = 'green', icon = fdIcon('star', lib = 'entypo')),
-  fdInfoBox('Residual Standard Error', d$deviance, 'on 21 degrees of freedom', 
+  fdInfoBox('Residual Standard Error', 2.65, 'on 21 degrees of freedom', 
     color = 'purple', icon = fdIcon('flash', lib = 'entypo'))
 ) %>%
-  fdPreviewBoard(wrapBox = F)
+  fdPreview(wrapBox = F)
