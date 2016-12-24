e9e2c33bce4b102f52b2e6e178f8652bebb1f4a6
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-23 16:55:28 -0800

    add glmnet example to fdPanelCoefficients.

--- inst/examples/fdPanelCoefficients.R
+++ inst/examples/fdPanelCoefficients.R
@@ -1,8 +1,22 @@
 library(flightdeck)
+
+#' Example 1: Linear Regression Model
 data(diamonds, package = 'ggplot2')
-if (interactive()){
-  lm(price ~ ., data = diamonds) %>%
-    fdPanelCoefficients %>%
-    fdPreview  
+panel1 <- lm(price ~ ., data = diamonds) %>%
+  fdPanelCoefficients %>%
+  fdPreview  
+
+if (interactive()) {
+  panel1
 }
 
+#' Example 2: GlmNet Model
+library(glmnet)
+data(QuickStartExample)
+panel2 <- glmnet(x, y) %>%
+  fdPanelCoefficients(s = 0.3) %>%
+  fdPreview
+
+if (interactive()){
+  panel2
+}
