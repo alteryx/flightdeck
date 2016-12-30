7946369181d4abee50368bd7faf3d3f31dc48015
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-29 22:26:32 -0800

    update fdPlotConfusionMatrix.

--- inst/examples/fdPlotConfusionMatrix.R
+++ inst/examples/fdPlotConfusionMatrix.R
@@ -3,11 +3,13 @@ library(rpart)
 mod <- rpart(Species ~ ., data = iris)
 outcome <- as.character(attr(mod, 'ylevels')[mod$y])
 pred <- predict(mod, type = "class")
-cmat <- as.data.frame.matrix(table(outcome, pred))
+
+# Example 1: Table
+ctable <- table(outcome, pred)
 
 if (interactive()){
   fdRowBox(width = 6,
-    fdPlotConfusionMatrix(cmat),
+    fdPlotConfusionMatrix(ctable),
     title = 'Confusion Matrix',
     footer = paste(
       'Numbers in parantheses indicate % of observations', 
@@ -16,5 +18,17 @@ if (interactive()){
   ) %>% 
   fdPreview(wrap = 'none')
 }
+
+# Example 2: Matrix
+cmat2 <- matrix(data = c(63, 3, 4, 33), nrow = 2, ncol = 2,
+  dimnames = list(
+    c('Actual Positive', 'Actual Negative'),
+    c('Predicted Positive', 'Predicted Negative')
+  )
+)
+
+fdPlotConfusionMatrix(cmat2) %>%
+  fdPreview
+
 
 
