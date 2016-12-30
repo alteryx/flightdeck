02a036818aad8e7cb795935fe54af746451035aa
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-29 17:19:44 -0800

    update flightdeck css and js.

--- inst/htmlwidgets/lib/flightdeck/flightdeck.js
+++ inst/htmlwidgets/lib/flightdeck/flightdeck.js
@@ -6,4 +6,13 @@ $(document).ready(function(){
     $('#' + $(this).data('page')).show()
     window.dispatchEvent(new Event('resize'));
   })
+  
+   // confusion matrices
+   $('.table.fd-confusion-matrix td').css('background', function(i, v){
+     var v2 = $($(this).children('div')[0]).data('value')
+     var v3 = $($(this).children('div')[0]).data('phrase')
+     return v3 === 'correctly'  ? 
+       'rgba(44, 160, 44, ' + v2 + ')' : 
+       'rgba(214, 39, 40,  ' + v2 + ')'
+  })
 })
