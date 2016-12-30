79eb1fcd1f1d28773e6830e39575c585f9252c8f
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-30 15:36:55 -0800

    use makeHtmlId to construct valid id.
    i need to rethink the API here. it might make sense to run it through makeHtmlId by default.

--- inst/examples/fdTabs.R
+++ inst/examples/fdTabs.R
@@ -4,7 +4,7 @@ library(htmltools)
 # Example 1
 if (interactive()){
   # Example 1
-  fdTabsetPanel(selected = 'Tab2',
+  fdTabsetPanel(selected = makeHtmlId('Tab2'),
     fdTabPanel('Tab1', h3("This is tab 1")),
     fdTabPanel('Tab2', h3("This is tab 2"))
   ) %>%
