92980ab90dbac8b12d36498535c5f6d2b672c387
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-01-03 02:22:20 -0800

    add stub for package level documentation.

--- R/flightdeck-package.r
+++ R/flightdeck-package.r
@@ -2,6 +2,7 @@
 #'
 #' @name flightdeck
 #' @docType package
-#' importFrom graphics cdplot
-#' importFrom stats coef loess.smooth qqnorm residuals rpois runif xtabs
-#' importFrom utils getFromNamespace packageVersion read.csv
+#' @importFrom graphics cdplot
+#' @importFrom stats coef loess.smooth qqnorm residuals rpois runif xtabs
+#' @importFrom utils getFromNamespace packageVersion read.csv
+NULL
