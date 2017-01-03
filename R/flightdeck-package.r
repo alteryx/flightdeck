aacdc5372ec78afc1439766330dc7ed293080c50
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-01-03 01:46:12 -0800

    initial commit of package doc.

---
+++ R/flightdeck-package.r
@@ -0,0 +1,7 @@
+#' flightdeck.
+#'
+#' @name flightdeck
+#' @docType package
+#' importFrom graphics cdplot
+#' importFrom stats coef loess.smooth qqnorm residuals rpois runif xtabs
+#' importFrom utils getFromNamespace packageVersion read.csv
