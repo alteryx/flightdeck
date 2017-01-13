f2deb2d58a5288e122c2883de665690234d80063
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-01-12 16:02:33 -0800

    import cov and var from stats package.

--- R/flightdeck-package.r
+++ R/flightdeck-package.r
@@ -1,8 +1,9 @@
 #' flightdeck.
-#'
+#' 
 #' @name flightdeck
 #' @docType package
 #' @importFrom graphics cdplot
-#' @importFrom stats coef loess.smooth qqnorm residuals rpois runif xtabs
+#' @importFrom stats coef loess.smooth qqnorm residuals rpois runif xtabs cov
+#'   var
 #' @importFrom utils getFromNamespace packageVersion read.csv
 NULL
